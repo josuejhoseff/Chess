aba122d0eb1818913831b90a34ad2cc63c2c5337
josuejhoseff <josuejhosefff@gmail.com>
2022-02-10 10:09:06 -0300

    Chess layer e mostrar o tabuleiro

--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,12 @@
+import Chess.ChessMath;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		
-
+		ChessMath chessMath = new ChessMath();
+		UI.printBoard(chessMath.getPieces());
 	}
 
 }

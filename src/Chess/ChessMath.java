81861fed69a08bb628baa18b5b153f5092404334
josuejhoseff <josuejhosefff@gmail.com>
2022-02-11 14:40:15 -0300

    BoardException e programação defensiva

--- src/Chess/ChessMath.java
+++ src/Chess/ChessMath.java
@@ -3,18 +3,19 @@ package Chess;
 import Chess.pieces.King;
 import Chess.pieces.Rook;
 import boardLayer.Board;
+import boardLayer.BoardException;
 import boardLayer.Position;
 
 public class ChessMath {
 
 	private Board board;
 
-	public ChessMath() {
+	public ChessMath() throws BoardException {
 		board = new Board(8, 8);
 		initialSetup();
 	}
 
-	public ChessPiece[][] getPieces() {
+	public ChessPiece[][] getPieces() throws BoardException {
 
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i = 0; i < board.getRows(); i++) {
@@ -26,8 +27,8 @@ public class ChessMath {
 
 	}
 	
-	public void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+	public void initialSetup() throws BoardException {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(5, 1));
 		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
 		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
 	}
--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,9 @@
 import Chess.ChessMath;
+import boardLayer.BoardException;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws BoardException {
 		
 		
 		ChessMath chessMath = new ChessMath();
